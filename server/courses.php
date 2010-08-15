cd5168d583fc8674ded7d25f98f8b8bde188c877
Chris Cooper <chriscooper1991@gmail.com>
2010-08-14 23:17:36 -0400

    Removed server from initial design

--- server/courses.php
+++
@@ -1,9 +0,0 @@
-<?php
-    echo "You are on Chris' test server.<br><br>";
-    if(@$_GET['option']) {
-        echo "Your test option was \"{$_GET['option']}\"";
-    }
-    exec("python CourseScraper.py")
-	//course_entries.data
-	
-?>
