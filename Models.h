ae017e2a2a01ff33a50ee6b2dccb8f5f0c529516
Chris Cooper <chriscooper1991@gmail.com>
2010-09-06 22:34:10 -0400

    Added model header file for easy importing

---
+++ Models.h
@@ -0,0 +1,12 @@
+/*
+ *  Models.h
+ *  QWin Mac
+ *
+ *  Created by Chris Cooper on 10-09-06.
+ *  Copyright 2010 Doliaris Software Consulting Ltd. All rights reserved.
+ *
+ */
+
+#import "Course.h"
+#import "Section.h"
+#import "TimePeriod.h"
