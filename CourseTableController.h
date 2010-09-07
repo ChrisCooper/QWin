a8e6f0160f0e04e98aac0a2809f07b05d47a9ff1
Chris Cooper <chriscooper1991@gmail.com>
2010-09-06 22:37:52 -0400

    Added empty table controller to be delegate and data source for the course browser

---
+++ CourseTableController.h
@@ -0,0 +1,16 @@
+//
+//  CourseTableController.h
+//  QWin Mac
+//
+//  Created by Chris Cooper on 10-09-06.
+//  Copyright 2010 Doliaris Software Consulting Ltd. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface CourseTableController : NSObject <NSTableViewDataSource, NSTableViewDelegate> {
+
+}
+
+@end
---
+++ CourseTableController.m
@@ -0,0 +1,14 @@
+//
+//  CourseTableController.m
+//  QWin Mac
+//
+//  Created by Chris Cooper on 10-09-06.
+//  Copyright 2010 Doliaris Software Consulting Ltd. All rights reserved.
+//
+
+#import "CourseTableController.h"
+
+
+@implementation CourseTableController
+
+@end
